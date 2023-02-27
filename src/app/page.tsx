ed602d191d7d6f7c5a87830b662691989d4ae81c
Gabriel Ribeiro <ribeirogabx@gmail.com>
2023-02-27 18:53:16 -0300

    refactor: comment request to first deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { Container } from '@/components/Container';
 import { CopyButton } from '@/components/CopyButton';
-import { publicUrl } from '@/constants';
+// import { publicUrl } from '@/constants';
 
 export default async function Home() {
-  const response = await fetch(`${publicUrl}/api/phrases`, {
-    method: 'GET',
-    next: {
-      revalidate: 10,
-    },
-  });
-  const { phrase } = await response.json();
+  // const response = await fetch(`${publicUrl}/api/phrases`, {
+  //   method: 'GET',
+  //   next: {
+  //     revalidate: 10,
+  //   },
+  // });
+  // const { phrase } = await response.json();
+
+  const phrase = 'first test';
 
   return (
     <Container className="relative flex h-[100vh] flex-col justify-between overflow-hidden">
